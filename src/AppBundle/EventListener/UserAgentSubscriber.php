eecb8853f8e21a50efce5ccc57f3bce4619c8fa4
Jose Gonzalez <jose.gonzalez@webfg.com>
2016-08-04 13:24:11 +0200

    - kernel.request and the RouterListener

--- src/AppBundle/EventListener/UserAgentSubscriber.php
+++ src/AppBundle/EventListener/UserAgentSubscriber.php
@@ -5,7 +5,9 @@ namespace AppBundle\EventListener;
 
 use Psr\Log\LoggerInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
 
 class UserAgentSubscriber implements EventSubscriberInterface
 {
@@ -22,12 +24,19 @@ class UserAgentSubscriber implements EventSubscriberInterface
         $request = $event->getRequest();
         $userAgent = $request->headers->get('User-Agent');
         $this->logger->info('The user agent is: '.$userAgent);
+
+        if(rand(0, 100) > 50)
+        {
+            //$response = new Response('Come back later!');
+            //$event->setResponse($response);
+        }
     }
 
     public static function getSubscribedEvents()
     {
         return array(
-            'kernel.request' => 'onKernelRequest'
+            // kernel.request
+            KernelEvents::REQUEST => 'onKernelRequest'
         );
     }
 }
